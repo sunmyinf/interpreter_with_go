ba62a9a897396cdb3be1adf7703f6ffb09516801
taiyo <swimy.inf+github@gmail.com>
2019-08-19 23:56:47 +0900

    Implement parser for let

---
+++ monkey/ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import "github.com/sunmyinf/interpreter_with_go/monkey/token"
+
+// ASTの全てのノードはNode itfを実装しなければならない
+type Node interface {
+	// そのノードが関連づけられているトークンのリテラル値を返す
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET トークン
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// IdnetifierはExpression itfを実装する
+// 本来let文の識別子（Identifier）は値を生成しないので、本来はExpressionではないが
+// これは簡単のためである。例: let x = valueProducingIdentifier;
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
