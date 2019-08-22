0d4edd389946e5cae0b7140f7a6fb22055234d89
taiyo <swimy.inf+github@gmail.com>
2019-08-22 23:05:52 +0900

    Implement let parser and test

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/sunmyinf/interpreter_with_go/monkey/ast"
 	"github.com/sunmyinf/interpreter_with_go/monkey/lexer"
 	"github.com/sunmyinf/interpreter_with_go/monkey/token"
@@ -11,10 +13,15 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる
 	p.nextToken()
 	p.nextToken()
@@ -86,6 +93,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
